2d23fb1d740d5786332fc8b92db3dfa36975deb6
bo grigore <bogrigore@gmail.com>
2015-07-26 18:33:36 +0300

    2015-07-26

---
+++ load.js
@@ -0,0 +1,34 @@
+﻿var Critter = [];
+
+xmlParse = function(xml) {
+	var i = 0;
+	$(xml).find("critter").each(function() {
+		
+		Critter[i]={
+		name:($(this).find("name").text()),
+		pic:($(this).find("pic").text()),
+		sound:($(this).find("sound").text()),
+		voice:($(this).find("voice").text())
+		
+		};
+		
+		i++;
+		});
+		
+	};
+	
+$.ajax({
+		type: "GET",
+		url: "/critters/critters.xml",
+		dataType: "xml",
+		success: xmlParse,
+		async: false,
+		error: function(jqxhr,textStatus,errorThrown){console.log(jqxhr);consolelog(textStatus);console.log(errorThrown);}
+		
+	});		
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,116 +1,97 @@
-var score = 0;
-
-var main = function() {
-
-	$('#dog-snd').trigger('load');
-	$('#horse-snd').trigger('load');
-	$('#cow-snd').trigger('load');
-	$('#sheep-snd').trigger('load');
-	$('#clink-snd').trigger('load');
+function ShuffleArr(arr){
+	// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	// Fisher-Yates aka (Knuth)
 	
-	
-	RandomButNot();
-};
-
-function RandomButNot(what) {
-	
-	seed = Math.random();
-	if(what === "dog" 	&& (Math.floor(seed * 3) + 1  == 1)) {  Horse();} else
-	if(what === "dog" 	&& (Math.floor(seed * 3) + 1  == 2)) {  Cow(); 	} else
-	if(what === "dog" 	&& (Math.floor(seed * 3) + 1  == 3)) {  Sheep();} else
-	if(what === "horse" && (Math.floor(seed * 3) + 1  == 1)) {  Dog(); 	} else
-	if(what === "horse" && (Math.floor(seed * 3) + 1  == 2)) {  Cow();	} else
-	if(what === "horse" && (Math.floor(seed * 3) + 1  == 3)) {  Sheep();} else
-	if(what === "cow" 	&& (Math.floor(seed * 3) + 1  == 1)) {  Dog();  } else
-	if(what === "cow" 	&& (Math.floor(seed * 3) + 1  == 2)) {  Horse();} else
-	if(what === "cow" 	&& (Math.floor(seed * 3) + 1  == 3)) {  Sheep();} else
-	if(what === "sheep" && (Math.floor(seed * 3) + 1  == 1)) {  Dog();	} else
-	if(what === "sheep" && (Math.floor(seed * 3) + 1  == 2)) {  Horse();} else
-	if(what === "sheep" && (Math.floor(seed * 3) + 1  == 3)) {  Cow();	} else
-	if(Math.floor(seed * 4) + 1  == 1) {  Dog(); 						} else
-	if(Math.floor(seed * 4) + 1  == 2) {  Horse(); 						} else
-	if(Math.floor(seed * 4) + 1  == 3) {  Cow(); 						} else
-	if(Math.floor(seed * 4) + 1  == 4) {  Sheep(); 						}; 
-	}; 
-	
-function Dog() {
-	
-	var loop = setInterval(function() { 
-	
-	$('#dog-snd').trigger('play');
-	
-			}, 3000);
-	
-	$('#dog-btn').click(function() {
-			clearInterval(loop);
-			$('#dog-snd').prop('currentTime', 0).trigger('pause');
-			$('#clink-snd').trigger('play');
-			$('#dog-btn').off();
-			UpdateScore();
-			RandomButNot('dog');
-			});
-
-	
+	var current_index = arr.length, temporary_value, random_index;
+	while (0 !== current_index) {
+		random_index = Math.floor(Math.random() * current_index);
+		current_index -= 1;
 		
-};
-
-function Horse() {
-	
-	var loop = setInterval(function() { 
-	
-	$('#horse-snd').trigger('play');
-	
-			}, 3000);
-	
-	$('#horse-btn').click(function() {
-			clearInterval(loop);
-			$('#horse-snd').prop('currentTime', 0).trigger('pause');
-			$('#clink-snd').trigger('play');
-			$('#horse-btn').off();
-			UpdateScore();
-			RandomButNot('horse');
-			});
-};
-
-function Cow() {
-	
-	var loop = setInterval(function() { 
-	
-	$('#cow-snd').trigger('play');
-	
-			}, 3000);
-	
-	$('#cow-btn').click(function() {
-			clearInterval(loop);
-			$('#cow-snd').prop('currentTime', 0).trigger('pause');
-			$('#clink-snd').trigger('play');
-			$('#cow-btn').off();
-			UpdateScore();
-			RandomButNot('cow');
-			});
-};
-
-function Sheep() {
-	
-	var loop = setInterval(function() { 
-	
-	$('#sheep-snd').trigger('play');
-	
-			}, 3000);
-	
-	$('#sheep-btn').click(function() {
-			clearInterval(loop);
-			$('#sheep-snd').prop('currentTime', 0).triggerHandler('pause');
-			$('#clink-snd').trigger('play');
-			$('#sheep-btn').off();
-			UpdateScore();
-			RandomButNot('sheep');
-			});
-};
-
-function UpdateScore() {
-	score+=10;
+		temporary_value = arr[current_index];
+		arr[current_index] = arr[random_index];
+		arr[random_index] = temporary_value;
+}};
+function UpdateScore(s) {
+	score+=s;
 	$('#sky h2').addClass('score').text(score + 'p');
 	};
+	
+	
+function LoadAudio(arr) {
+	for(i = 0; i < arr.length; i+=1){
+		var sound_id = "sound" + i ; 
+		var sound_src= arr[i].sound; 
+		// var voice_id = "voice" +i;
+		// var voice_src = arr[i].voice;
+		
+		var sound = '<audio id="' +sound_id+ '">' + '<source src="' + sound_src + '" type="audio/wav"></audio>';
+		// var voice = '<audio id="' +voice_id+ '">' + '<source src="' + voice_src + '" type="audio/wav"></audio>';
+		
+		$(sound).appendTo('body');
+		// $(voice).appendTo('body');
+		arr[i].index = i;
+	}
+
+};
+
+
+function DrawCritters(arr){
+	$("#container").empty();
+	
+	for (i = 0; i < 4 ; i += 1){
+	
+	
+	var btn = '<div id = "btn' + i + '"></div>';
+	$(btn).appendTo("#container").addClass('critter').css({"background-image": "url(" + arr[i].pic + ")"});
+	}
+	seed = Math.floor(Math.random() * 3);
+	
+	
+	for(i = 0; i < 4; i += 1) {
+		
+		if (i == seed){
+			
+			sound_Loop = setInterval(function() { 
+			$("#sound"+arr[seed].index).prop('currentTime', 0).trigger('play');
+				
+	
+			}, 3000);
+			
+			$("#btn" +seed).on("click", function() {
+					clearInterval(sound_Loop);
+					$("#sound"+arr[seed].index).trigger('pause');
+					$("#clink").trigger('play');
+					UpdateScore(10);
+					ShuffleArr(Critter);
+					DrawCritters(arr);
+				});
+		}
+		else {
+		 $("#btn" +i).on("click", function() {
+			
+			$("#wrong").trigger("play");
+			UpdateScore(-3);
+		});
+		}
+		
+	}
+};
+
+
+
+
+var score = 0;
+
+
+var main = function() {
+	
+	LoadAudio(Critter);
+	
+	DrawCritters(Critter);
+	
+	
+	
+};
+
 
 $(document).ready(main);
